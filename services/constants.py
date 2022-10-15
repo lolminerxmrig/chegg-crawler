21442f0e4087fcc73a53e5e660a0f69559624e25
unknown <m.modallal99@hotmail.com>
2022-10-15 11:39:59 +0000

    Initial commit

---
+++ services/constants.py
@@ -0,0 +1,20 @@
+CHEGG_GENERAL_QUESTION_BASE_URL = "https://www.chegg.com/homework-help/questions-and-answers/"
+CHEGG_LOGIN_URL = "https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F"
+CHEGG_CHECK_LOGIN_URL = "https://www.chegg.com/my/account"
+CANNOT_LOGIN =  "Cannot login"
+
+SERVER_INSTANCE_ID_NOT_EXIST = "Cannot Find Server Instance"
+ANSWERS_SPLITTER = "QUIZPLUS_ANSWER_SPLITTER"
+NO_ACTIVE_USER = "No active user"
+GENERAL_ERROR = "General Error !!"
+N_QUESTIONS_CRAWLE_SUCCESSFULLY = "CRAWLE SUCCESSFULLY"
+CHANGE_IP_ADDRESS = "Change IP address"
+NO_QUESTION_YET="Sorry But there is no question yet, wait the crawling process"
+
+MONTHS = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
+
+
+# Question Processing
+QUESTION_IMAGES_FOLDER_NAME = "chegg_general_question_images"
+NUMBER_OF_QUESTIONS_TO_PROCESS =  200
+S3_IMAGES_BUCKET_NAME = 'dev-quizplus-cdn'
---
+++ tala2.py
@@ -0,0 +1,16 @@
+import time
+
+if __name__ == '__main__':
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    time.sleep(3)
+    print("talaaaaaaa")
+    print("talaaaaaaa")
+    print("talaaaaaaa")
