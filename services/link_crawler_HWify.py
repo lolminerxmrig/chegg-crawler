bbd93a004fb5591560bee9abfa9be13424515fe2
Administrator <talaskafi1998@gmail.com>
2022-10-21 15:18:00 +0000

    adding selenium + pyautogui search

--- services/link_crawler_HWify.py
+++ services/link_crawler_HWify.py
@@ -34,8 +34,7 @@ mysql_db_manager = MySqlDBManager('admin',
 class HWifyCrawler:
     def _start_crawling(self):
         flag = 0
-        driver = BrowserDriver().driver
-        # dx, dy = self.set_resolution()
+        dx, dy = self.set_resolution()
         time.sleep(1)
         while True:
             # Comment: Here the first K not-crawled questions retrived which is better than hitting everytime on DB to get first not crawled
@@ -43,12 +42,8 @@ class HWifyCrawler:
             try:
                 for question in questions:
                     flag = 1
-                    # get url from Database and copy it
-                    # webbrowser.get(chrome_path).open("https://homeworkify.net/")
-                    # go to 700 850 to the window of search URL
-                    # pyautogui.moveTo(dx * 700, dy * 850, 1)
-                    # pyautogui.typewrite(question.url)
-                    self.selenium_search(driver, question)
+                    self.pyautogui_search(dx,dy,question)
+                    # self.selenium_search(question)
 
 
 
@@ -66,10 +61,31 @@ class HWifyCrawler:
         dx, dy = image_service.get_resolution()
         return dx, dy
 
-    def pyautogui_search(self):
+    def pyautogui_search(self, dx, dy, question):
+        # get url from Database and copy it
+        webbrowser.get(chrome_path).open("https://homeworkify.net/")
+        pyautogui.moveTo(dx * 700, dy * 850, 1)
+        time.sleep(1)
+        pyautogui.click(button='left')
+        time.sleep(.3)
+        pyautogui.typewrite(question.url)
+        time.sleep(1)
+        pyautogui.moveTo(dx * 1250, dy * 850, 1)
+        pyautogui.click(button='left')
+        time.sleep(1)
+        html = self._save_web_page(dx, dy)
+        if html.find("We have solution for your question!") == -1:
+            #no answer
+            print("no answer")
+        else:
+            pyautogui.scroll(10)
+            time.sleep(30)
 
 
-    def selenium_search(self, driver, question):
+
+
+    def selenium_search(self, question):
+        driver = BrowserDriver().driver
         driver.get("https://homeworkify.net/")
         driver.maximize_window()
         time.sleep(2)
@@ -80,19 +96,39 @@ class HWifyCrawler:
         element.click()
         time.sleep(2)
         try:
-            status=driver.find_element(By.XPATH,'//*[@id="et-boc"]/div/div/div[1]/div[2]/div/div[1]/div/div[2]/div/h2').text
-            if status=='We have solution for your question!':
+            status = driver.find_element(By.XPATH,
+                                         '//*[@id="et-boc"]/div/div/div[1]/div[2]/div/div[1]/div/div[2]/div/h2').text
+            if status == 'We have solution for your question!':
                 # there is an answer, so we open it and store it
                 time.sleep(30)
                 element = driver.find_element(By.ID, 'view-solution')
                 element.click()
                 time.sleep(2)
-            elif status=='No solution found!':
+            elif status == 'No solution found!':
                 # there is no solution
                 print("there is No solution")
 
         except Exception as e:
             print("there is an Error")
 
-
-
+    def _save_web_page(self, dx, dy):
+        pyperclip.copy("")
+        while True:
+            print("clicking to get soruce html")
+            time.sleep(1)
+            pyautogui.hotkey('ctrl', 'u')
+            time.sleep(1)
+            pyautogui.moveTo(dx * 400, dy * 420, 1)
+            pyautogui.click(button='left')
+            # html = pyautogui.hotkey('ctrl', 'a')
+            time.sleep(1)
+            pyautogui.hotkey('ctrl', 'a')
+            time.sleep(1)
+            pyautogui.hotkey('ctrl', 'c')
+            time.sleep(1)
+            html = pyperclip.paste()
+            if html.find("<html") != -1:
+                break
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'w')
+        return html
